0f69a0a88e339f8341ebe3da7370a5455d1b6cd3
Kayla Blalock <kay.blalock91@gmail.com>
2020-07-05 11:36:43 -0400

    Added an array of questions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,45 @@
 var quizCont = document.getElementById("codeQuiz");
 var resultsCont = document.getElementById("results");
 var submitButton = document.getElementById("submit");
+
+quiz();
+
+function quiz() {}
+
+function showResults() {}
+
+
+var quizQuestions = [{
+        question: " Question 1",
+        answers: {
+            a: "Answer A",
+            b: "Answer B",
+            c: "Answer C"
+        },
+        //   place the correct answer in answer spot with corresponding letter below
+        correctAnswer: "b"
+    },
+    {
+        question: "Question 2",
+        answers: {
+            a: "Answer A",
+            b: "Answer B",
+            c: "Answer C"
+        },
+        //   place the correct answer in answer spot with corresponding letter below  
+        correctAnswer: "a"
+    },
+    {
+        question: "Question 3",
+        answers: {
+            a: "Answer A",
+            b: "Answer B",
+            c: "Answer C",
+        },
+        //   place the correct answer in answer spot with corresponding letter below
+        correctAnswer: "c"
+    }
+];
+
+
+submitButton.addEventListener('click', showResults);
