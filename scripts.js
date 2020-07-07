93e2e0b3eee28523f4a33cd1889faf3315c434db
Kayla Blalock <kay.blalock91@gmail.com>
2020-07-06 21:48:08 -0400

    Allow questions to display on page

--- scripts.js
+++ scripts.js
@@ -1,45 +1,54 @@
-var quizCont = document.getElementById("codeQuiz");
-var resultsCont = document.getElementById("results");
+var quizContent = document.getElementById("codeQuiz");
+var resultsContent = document.getElementById("results");
+var startButton = document.getElementById("start");
 var submitButton = document.getElementById("submit");
-
-quiz();
-
-function quiz() {}
-
-function showResults() {}
-
-
 var quizQuestions = [{
-        question: " Question 1",
-        answers: {
-            a: "Answer A",
-            b: "Answer B",
-            c: "Answer C"
-        },
+        question: "Question 1",
+        answers: ["Answer A", "Answer B", "Answer C"],
         //   place the correct answer in answer spot with corresponding letter below
-        correctAnswer: "b"
+        correctAnswer: "Answer B"
     },
     {
         question: "Question 2",
-        answers: {
-            a: "Answer A",
-            b: "Answer B",
-            c: "Answer C"
-        },
+        answers: ["Answer A", "Answer B", "Answer C"],
         //   place the correct answer in answer spot with corresponding letter below  
         correctAnswer: "a"
     },
     {
         question: "Question 3",
-        answers: {
-            a: "Answer A",
-            b: "Answer B",
-            c: "Answer C",
-        },
+        answers: ["Answer A", "Answer B", "Answer C"],
         //   place the correct answer in answer spot with corresponding letter below
         correctAnswer: "c"
     }
 ];
 
+startButton.addEventListener("click", startQuiz);
 
-submitButton.addEventListener('click', showResults);
+function startQuiz() {
+    var currentQuestion = quizQuestions[0];
+    displayQuestion(currentQuestion);
+}
+
+function displayQuestion(quizQuestion) {
+    var questionText = document.createTextNode(quizQuestion.question);
+    quizContent.appendChild(questionText);
+
+    quizQuestion.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.textContent = answer;
+        quizContent.appendChild(button);
+        button.addEventListener("click", () => handleButtonClick(answer, quizQuestion.correctAnswer));
+    });
+    document.createElement("button");
+}
+
+function handleButtonClick(answer, correctAnswer) {
+    if (answer === correctAnswer) {
+        //Update page with Right! and increase score
+        resultsContent.textContent = "Correct Answer";
+    } else {
+        // Update page with Wrong! and decrease decrement on the clock
+        resultsContent.textContent = "Wrong Answer";
+    }
+
+}
